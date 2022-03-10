44b9473152c01b4ed034a66a34efca520ae3ff29
Andrea de Oliveira <soudkova.a@gmail.com>
2022-03-10 23:40:03 +0100

    correction of width/height of image variables

--- main.js
+++ main.js
@@ -22,12 +22,12 @@ hunterX = getTheHeight / 2;
 hunterY = getTheWidth / 2;
 
 const imgCoin = document.getElementById('mince');
-coinWidth = imgCoin.clientWidth;
-coinHeight = imgCoin.clientHeight;
+coinWidth = imgCoin.width;
+coinHeight = imgCoin.height;
 
 const imgHunter = document.getElementById('panacek');
-hunterWidth = imgHunter.clientWidth;
-hunterHeight = imgHunter.clientHeight;
+hunterWidth = imgHunter.width;
+hunterHeight = imgHunter.height;
 //console.log(hunterWidth)
 //console.log(hunterHeight)
 
@@ -58,33 +58,55 @@ function byClickingTheKey(event) {
 	if (event.key === "ArrowLeft") { 
 		hunterY -= 10;
 		hunter.style.left = hunterY + "px";
+		if (hunterY < 0) {
+			hunterY = 0;
+		}
+
 		console.log("doleva");
 
 	} else if (event.key === "ArrowRight") { 
 		hunterY += 10;
 		hunter.style.left = hunterY + "px";
+		if (hunterY + hunterWidth > window.innerWidth) {
+			hunterY = window.innerWidth - hunterWidth;
+		}
 		console.log("doprava");
 
 	} else if (event.key === "ArrowUp") { 
 		hunterX = hunterX - 10;
 		hunter.style.top = hunterX + "px";
+		if (hunterX < 0) {
+			hunterX = 0;
+		}
 		console.log("nahoru");
 
 	} else if (event.key === "ArrowDown") {  
 		hunterX = hunterX + 10;
 		hunter.style.top = hunterX + "px";
+		if (hunterX + hunterHeight > window.innerHeight) {
+			hunterX = window.innerHeight - hunterHeight;
+		}
 		console.log("dolu");
 
 	}
+	testTheCollision();
 }
 
 function testTheCollision() {
-	if (!( hunterX + hunterWidth < coinX || coinX + coinWidth < hunterX || hunterY + hunterHeight < coinY || coinY + coinHeight < hunterY)) {
+	if (
+		!( 
+			hunterX + hunterWidth < coinX || 
+			coinX + coinWidth < hunterX || 
+			hunterY + hunterHeight < coinY || 
+			coinY + coinHeight < hunterY
+		)
+	) {
 		// panacek a mince se prekryvaji
 		let score = document.getElementById('score');
 		score = 0;
 		score = score + 1;
 		console.log(score);
-
+		newCoin();
 	}
 }
+
